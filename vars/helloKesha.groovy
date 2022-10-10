d2e5c4a5d8240081afbd72205d9a7ffea16d804c
Казанцев Владимир Сергеевич <Vl.Kazantsev@ase-ec.ru>
2022-10-10 08:56:01 +0300

    Изменены скобки

--- vars/helloKesha.groovy
+++ vars/helloKesha.groovy
@@ -1,14 +1,14 @@
 #!/usr/bin/env groovy
 
-def call(Map config){
-	pipeline{
-		agent any
-		stages{
-			stage("Этап Первый"){
-				steps{
-					sh "echo Privet ${config.name} po familii ${config.lastname}"
-				}
-			}
-		}
-	}
+def call(Map config) {
+  pipeline {
+    agent any
+    stages {
+      stage("Этап Первый") {
+        steps {
+          sh "echo Privet ${config.name} po familii ${config.lastname}"
+        }
+      }
+    }
+  }
 }
