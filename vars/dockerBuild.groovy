6304e0a41181506c51fd2bc31f7762de22195b42
Казанцев Владимир Сергеевич <Vl.Kazantsev@ase-ec.ru>
2022-10-23 16:25:10 +0300

    Dobavil linux script

--- vars/dockerBuild.groovy
+++ vars/dockerBuild.groovy
@@ -8,7 +8,8 @@ def call() {
 		stages {
 			stage("Load Dockerfile") {
 				steps {
-					def dockerfilecontents = libraryResource "docker/Dockerfile"
+					//def dockerfilecontents = libraryResource "docker/Dockerfile"
+					def dockerfilecontents = libraryResource "scripts/linux/hello-world.sh"
 					writeFile file : 'Dockerfile', text: dockerfilecontents
 					sh "pwd"
 					sh "ls"
