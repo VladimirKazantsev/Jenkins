e45cc482b5a5a8431f51950088cab4aaf2409ed0
Казанцев Владимир Сергеевич <Vl.Kazantsev@ase-ec.ru>
2022-10-23 14:00:06 +0300

    Добавил докер файл

--- vars/dockerBuild.groovy
+++ vars/dockerBuild.groovy
@@ -8,10 +8,16 @@ def call(Map config) {
 			timestamps()
 		}
 		stages {
+			stage("Load Dockerfile") {
+				steps {
+					def dockerfilecontents = libraryResource "docker/Dockerfile"
+					writeFile file : Dockerfile, text: dockerfilecontents
+				} 
+			}
 			stage("Create Docker image") {
 				steps {
 					script {
-						docker.build -f 
+						docker.build .
 					}
 				}
 			}
