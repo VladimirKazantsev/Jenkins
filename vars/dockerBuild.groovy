c5189bf52355c678c04b07cd675aa61e5032011c
Казанцев Владимир Сергеевич <Vl.Kazantsev@ase-ec.ru>
2022-10-23 16:16:52 +0300

    Comment def

--- vars/dockerBuild.groovy
+++ vars/dockerBuild.groovy
@@ -8,8 +8,8 @@ def call() {
 		stages {
 			stage("Load Dockerfile") {
 				steps {
-					def dockerfilecontents = libraryResource "docker/Dockerfile"
-					writeFile file : Dockerfile, text: dockerfilecontents
+					// def dockerfilecontents = libraryResource "docker/Dockerfile"
+					// writeFile file : Dockerfile, text: dockerfilecontents
 					sh "pwd"
 					sh "ls"
 				} 
