32114dacc21607d4e61fc27f4f75ccb572ca09d6
Казанцев Владимир Сергеевич <Vl.Kazantsev@ase-ec.ru>
2022-10-23 15:04:15 +0300

    Vse ubral

--- vars/helloWorld.groovy
+++ vars/helloWorld.groovy
@@ -1,13 +1,4 @@
 #!/usr/bin/env groovy
 def call() {
-	// pipeline {
-	// 	agent any
-	// 	stages {
-	// 		stage("Первый этап") {
-	// 			steps {
-					sh "echo Hello world"
-				}
-// 			}
-// 		}
-// 	}
-// }
+	sh "echo Hello world"
+}
