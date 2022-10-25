e2206cdb3ef19d10b916e2d1d99ff239410ea9b4
Казанцев Владимир Сергеевич <Vl.Kazantsev@ase-ec.ru>
2022-10-25 20:00:31 +0300

    2

--- vars/backupPGSQL.groovy
+++ vars/backupPGSQL.groovy
@@ -18,7 +18,7 @@ def call() {
 			stage("Ensure source folder exist") {
 				steps {
 					sh """ 
-						ssh jenkins@192.168.50.230 'bash -s' << 'ENDSSH
+						ssh jenkins@192.168.50.230 'bash -s' << 'ENDSSH'
 						
 						hostname
 						
