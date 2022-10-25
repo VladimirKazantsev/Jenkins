51a6fb0958a23d64032e8d56d753967e1519842b
Казанцев Владимир Сергеевич <Vl.Kazantsev@ase-ec.ru>
2022-10-25 19:28:37 +0300

    Added backup

---
+++ vars/backupPGSQL.groovy
@@ -0,0 +1,31 @@
+#!/usr/bin/env groovy
+def call() {
+	pipeline {
+		agent any
+		options {
+			timestamps()
+		}
+		parameters {
+			
+			string(
+				name: 'dbServer',
+				defaultValue: 'ubnt',
+				description: 'Адрес сервера с pgsql'
+			)
+		}
+
+		stages {
+			stage("Ensure source folder exist") {
+				steps {
+					sh """ 
+						ssh jenkins@192.168.50.230 'bash -s << 'ENDSSH'
+						
+						hostname
+						
+						ENDSSH'
+					"""
+				} 
+			}
+		}
+	}
+}
