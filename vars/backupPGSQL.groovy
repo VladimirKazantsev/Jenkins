23710ffb93f2023ab2f14b1685a74b4cfacdc107
Казанцев Владимир Сергеевич <Vl.Kazantsev@ase-ec.ru>
2022-10-26 15:19:50 +0300

    3

--- vars/backupPGSQL.groovy
+++ vars/backupPGSQL.groovy
@@ -18,14 +18,14 @@ def call() {
 			stage("Ensure source folder exist") {
 				steps {
 					sh """
-						// ssh jenkins@192.168.50.230 'bash -s << 'ENDSSH'
+						ssh jenkins@192.168.50.230 'bash -s << 'ENDSSH'
 						// echo =================Имя хоста===================
 						// hostname
 						// echo =============Рабочая директория==============
 						// pwd
 						// echo =============Образы докер==============
 						// docker images
-						//docker exec pg5432 pg_dump -U postgres elets > /backup/4.backup
+						docker exec pg5432 pg_dump -U postgres elets > /backup/4.backup
 						ls /backup
 						
 ENDSSH'
