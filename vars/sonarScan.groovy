8ccd018a19067ae64ee413dc751c785b12751392
Казанцев Владимир Сергеевич <Vl.Kazantsev@ase-ec.ru>
2022-11-02 16:51:05 +0300

    Replace item

--- vars/sonarScan.groovy
+++ vars/sonarScan.groovy
@@ -18,7 +18,7 @@ def call() {
 					def datas = readYaml file: 'repos.yml'
 					datas.each {
 
-						println "${it}"
+						println "${el}"
 					}
 					// writeFile file : 'Dockerfile', text: dockerfilecontents
 					sh "pwd"
