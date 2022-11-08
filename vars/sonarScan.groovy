58a0575523af78287bf744f8d8995abfd0622603
Казанцев Владимир Сергеевич <Vl.Kazantsev@ase-ec.ru>
2022-11-08 10:09:09 +0300

    Added char

--- vars/sonarScan.groovy
+++ vars/sonarScan.groovy
@@ -60,6 +60,7 @@ def call() {
 				steps {
 					withSonarQubeEnv(installationName: 'sonarqubeElets') { 
           sh './mvnw clean org.sonarsource.scanner.maven:sonar-maven-plugin:3.9.0.2155:sonar'
+					}
 				}
       }
 		}
