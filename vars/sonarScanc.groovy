81b603856083913bd80c81263a3ae3671f988fc3
Казанцев Владимир Сергеевич <Vl.Kazantsev@ase-ec.ru>
2022-11-10 18:38:31 +0300

    Added scan

---
+++ vars/sonarScanc.groovy
@@ -0,0 +1,49 @@
+#!/usr/bin/env groovy
+def call() {
+	pipeline {
+		agent any
+		
+		options {
+			timestamps()
+		}
+		
+		stages {
+			
+			stage("SonarQube scan") {
+				
+				steps {
+					echo "===============Scan Sonar======================="
+					
+					script {
+						git	url: "git@github.com:VladimirKazantsev/c-app.git"
+						def scannerHome = tool 'SonarQube-1'
+
+						withSonarQubeEnv(installationName: 'sonarqubeElets') {
+						sh "dotnet ${scannerHome}/SonarScanner.MSBuild.dll begin /k:\"vladimir\""
+						sh "dotnet build"
+						sh "dotnet ${scannerHome}/SonarScanner.MSBuild.dll end"
+						}
+						sh "pwd"
+						sh "ls"
+						sh "tree"
+					}
+				} 
+			}
+			
+			stage("Example") {
+				steps {
+					script {
+						
+							try {
+									sh 'ls truba'
+							}
+							catch (all) {
+									echo 'Neverno zadana direktory'
+							}
+						
+					}
+				}
+      }
+		}
+	}
+}
