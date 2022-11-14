41605f7bbd4fe208b37fd6ef54fad4390cb049f2
Казанцев Владимир Сергеевич <Vl.Kazantsev@ase-ec.ru>
2022-11-14 11:46:27 +0300

    Added  dotnet linux

--- vars/sonarScanc.groovy
+++ vars/sonarScanc.groovy
@@ -24,13 +24,15 @@ def call() {
 						withSonarQubeEnv(installationName: 'sonarqubeElets') {
 						
 						// sh "dotnet /var/lib/jenkins/.dotnet/tools/.store/dotnet-sonarscanner/5.8.0/dotnet-sonarscanner/5.8.0/tools/netcoreapp3.0/any/SonarScanner.MSBuild.dll begin  /k:\"testVladimir\" /d:sonar.verbose=true /d:sonar.login="sqp_5111c5b096cf74f8f17cccb9c0bd3d8de1ebf943""
-						// sh "dotnet build Helloworld.csproj"
+						sh "dotnet /var/lib/jenkins/.dotnet/tools/.store/dotnet-sonarscanner/5.8.0/dotnet-sonarscanner/5.8.0/tools/netcoreapp3.0/any/SonarScanner.MSBuild.dll begin /k:\"testVladimir\" /d:sonar.host.url="http://192.168.50.20:9000" /d:sonar.login="sqp_5111c5b096cf74f8f17cccb9c0bd3d8de1ebf943""
+						sh "dotnet build"
+						sh "dotnet /var/lib/jenkins/.dotnet/tools/.store/dotnet-sonarscanner/5.8.0/dotnet-sonarscanner/5.8.0/tools/netcoreapp3.0/any/SonarScanner.MSBuild.dll end /d:sonar.login="sqp_5111c5b096cf74f8f17cccb9c0bd3d8de1ebf943""
 						// sh "dotnet ${scannerHome}/SonarScanner.MSBuild.dll end"
 
-						sh "dotnet ${scannerHome}/SonarScanner.MSBuild.dll begin /d:sonar.verbose=true /k:\"testVladimir\" /d:sonar.verbose=true /d:sonar.login="sqp_5111c5b096cf74f8f17cccb9c0bd3d8de1ebf943""
-						sh "dotnet build Helloworld.csproj"
-						sh "dotnet test Helloworld.csproj"
-						sh "dotnet ${scannerHome}/SonarScanner.MSBuild.dll end"
+						// sh "dotnet ${scannerHome}/SonarScanner.MSBuild.dll begin /d:sonar.verbose=true /k:\"testVladimir\" /d:sonar.verbose=true /d:sonar.login="sqp_5111c5b096cf74f8f17cccb9c0bd3d8de1ebf943""
+						// sh "dotnet build Helloworld.csproj"
+						// sh "dotnet test Helloworld.csproj"
+						// sh "dotnet ${scannerHome}/SonarScanner.MSBuild.dll end"
 						}
 						sh "pwd"
 						sh "ls"
