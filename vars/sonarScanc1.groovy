a998dacbe3919ee078afdfa6236c23c9ade4b5f1
Казанцев Владимир Сергеевич <Vl.Kazantsev@ase-ec.ru>
2022-11-25 17:35:14 +0300

    Удалил все коментарии

---
+++ vars/sonarScanc1.groovy
@@ -0,0 +1,51 @@
+#!/usr/bin/env groovy
+def call() {
+	pipeline {
+		agent {
+			label 'Glavnyi'
+		} 
+		
+		options {
+			timestamps()
+		}
+		
+		stages {
+			
+			stage("SonarQube scan") {
+				
+				steps {
+					echo "===============Scan Sonar======================="
+					
+					script {
+						git branch: "main",
+								url: "git@github.com:VladimirKazantsev/c-app.git"
+						def scannerHome = tool 'SonarQube-1'
+						sh "ls"
+						sh "pwd"
+						println "${scannerHome}"
+						withSonarQubeEnv(installationName: 'sonarqubeElets') {
+						sh "dotnet ${scannerHome}/SonarScanner.MSBuild.dll begin /k:\"testVladimir\" /d:sonar.verbose=true /d:sonar.login=\"sqp_5111c5b096cf74f8f17cccb9c0bd3d8de1ebf943\""
+						sh "dotnet build Helloworld.csproj"
+						sh "dotnet ${scannerHome}/SonarScanner.MSBuild.dll end /d:sonar.login=\"sqp_5111c5b096cf74f8f17cccb9c0bd3d8de1ebf943\""
+						}
+					}
+				} 
+			}
+			
+			stage("Example") {
+				steps {
+					script {
+						
+							try {
+									sh 'ls'
+							}
+							catch (all) {
+									echo 'Neverno zadana direktory'
+							}
+						
+					}
+				}
+      }
+		}
+	}
+}
