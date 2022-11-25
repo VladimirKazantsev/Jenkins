8b470b2e70f4550d8d7014206f272149a6a563c5
Казанцев Владимир Сергеевич <Vl.Kazantsev@ase-ec.ru>
2022-11-25 14:46:53 +0300

    ddd

--- vars/sonarScan.groovy
+++ vars/sonarScan.groovy
@@ -25,7 +25,7 @@ def call() {
 					def branchname =""
 					def logfiles = datas.toString().trim()
 					println "${logfiles}"
-					if (logfiles.contains('wwwwww')) {
+					if (logfiles.contains('wwwwww') || logfiles.contains('blue')) {
              echo "it's UnifiedSchedule"
 					 }
 					datas.each {
