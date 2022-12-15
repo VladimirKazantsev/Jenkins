c9d77ad50f13992cc5667bb4f2de6e5a896ad55e
Казанцев Владимир Сергеевич <Vl.Kazantsev@ase-ec.ru>
2022-12-15 15:06:20 +0300

    Deploy service

---
+++ vars/deployService.groovy
@@ -0,0 +1,48 @@
+#!/usr/bin/env groovy
+def call() {
+	pipeline {
+		agent any
+		
+		options {
+		
+			timestamps()
+		
+		}
+		parameters {
+
+			choice(name: 'IpServer',
+				choices: [
+					'Никуда',
+					'192.168.50.231'
+				],
+				description: 'Адрес сервера куда пойдет по ssh'
+
+			)
+			
+			string(
+				name: 'dbServer',
+				defaultValue: 'ubnt',
+				description: 'Адрес сервера с pgsql'
+			)
+		}
+
+		stages {
+			stage("Deploy conteiner") {
+				steps {
+					sh """
+						ssh jenkins@${params.IpServer} 'bash -s << 'ENDSSH'
+						echo =================Развертывание контейнера===================
+						docker run -d \
+						--name moskow \
+						--restart always \
+						--p 80:80 \
+						nginx
+						
+						
+ENDSSH'
+"""
+				} 
+			}
+		}
+	}
+}
