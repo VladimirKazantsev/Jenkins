28c988a0f3f72a8b4d19a38d2e01a6dc3b4e6066
Казанцев Владимир Сергеевич <Vl.Kazantsev@ase-ec.ru>
2022-12-22 09:59:35 +0300

    aaa

--- vars/helloKesha.groovy
+++ vars/helloKesha.groovy
@@ -1,19 +1,19 @@
 #!/usr/bin/env groovy
 
-def call(Map config) {
+def call(Map configm) {
   pipeline {
     agent any
     stages {
       stage("Этап Первый") {
         steps {
-          sh "echo Privet ${config.name} po familii ${config.lastname}"
+          sh "echo Privet ${configm.name} po familii ${configm.lastname}"
         }
       }
       stage("Этап Второй") {
         steps {
 					sh "pwd"
 					sh "mkdir -p ./Distrib/Config"
-          sh "echo Privet ${config.name} po familii ${config.lastname}"
+          sh "echo Privet ${configm.name} po familii ${configm.lastname}"
         }
       }
     }
